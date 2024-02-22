9d1b4bad8184cd3490e603c2d2ec001acedb930a
Filipe Patricio <facpatricio@gmail.com>
2024-02-22 15:28:52 +0100

    Add MarketDataService

--- SwiftUICrypto/Extensions/Double.swift
+++ SwiftUICrypto/Extensions/Double.swift
@@ -17,9 +17,9 @@ extension Double{
         let formatter = NumberFormatter()
         formatter.usesGroupingSeparator = true
         formatter.numberStyle = .currency
-        //formatter.locale = .current // <- default value
-        //formatter.currencyCode = "usd" // <- change currency
-        //formatter.currencySymbol = "$" // <- change currency symbol
+        formatter.locale = .current // <- default value
+        formatter.currencyCode = "usd" // <- change currency
+        formatter.currencySymbol = "$" // <- change currency symbol
         formatter.minimumFractionDigits = 2
         formatter.maximumFractionDigits = 2
         return formatter
@@ -45,9 +45,9 @@ extension Double{
         let formatter = NumberFormatter()
         formatter.usesGroupingSeparator = true
         formatter.numberStyle = .currency
-//        formatter.locale = .current // <- default value
-//        formatter.currencyCode = "usd" // <- change currency
-//        formatter.currencySymbol = "$" // <- change currency symbol
+        formatter.locale = .current // <- default value
+        formatter.currencyCode = "usd" // <- change currency
+        formatter.currencySymbol = "$" // <- change currency symbol
         formatter.minimumFractionDigits = 2
         formatter.maximumFractionDigits = 6
         return formatter
@@ -79,4 +79,44 @@ extension Double{
     func asPercentString() -> String {
         return asNumberString() + "%"
     }
+    
+    /// Convert a Double to a String with K, M, Bn, Tr abbreviations.
+    /// ```
+    /// Convert 12 to 12.00
+    /// Convert 1234 to 1.23K
+    /// Convert 123456 to 123.45K
+    /// Convert 12345678 to 12.34M
+    /// Convert 1234567890 to 1.23Bn
+    /// Convert 123456789012 to 123.45Bn
+    /// Convert 12345678901234 to 12.34Tr
+    /// ```
+    func formattedWithAbbreviations() -> String {
+        let num = abs(Double(self))
+        let sign = (self < 0) ? "-" : ""
+
+        switch num {
+        case 1_000_000_000_000...:
+            let formatted = num / 1_000_000_000_000
+            let stringFormatted = formatted.asNumberString()
+            return "\(sign)\(stringFormatted)Tr"
+        case 1_000_000_000...:
+            let formatted = num / 1_000_000_000
+            let stringFormatted = formatted.asNumberString()
+            return "\(sign)\(stringFormatted)Bn"
+        case 1_000_000...:
+            let formatted = num / 1_000_000
+            let stringFormatted = formatted.asNumberString()
+            return "\(sign)\(stringFormatted)M"
+        case 1_000...:
+            let formatted = num / 1_000
+            let stringFormatted = formatted.asNumberString()
+            return "\(sign)\(stringFormatted)K"
+        case 0...:
+            return self.asNumberString()
+
+        default:
+            return "\(sign)\(self)"
+        }
+    }
+
 }
