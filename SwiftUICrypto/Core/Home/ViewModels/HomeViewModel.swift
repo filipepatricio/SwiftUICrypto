b40cf7af7d07cc6f86141501c9e1a5d7a378b15e
Filipe Patricio <facpatricio@gmail.com>
2024-02-22 14:27:30 +0100

    Add filter coins

--- SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUICrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -12,6 +12,7 @@ class HomeViewModel: ObservableObject{
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
+    
     @Published var searchText = ""
     
     private let dataService = CoinDataService()
@@ -22,10 +23,28 @@ class HomeViewModel: ObservableObject{
     }
     
     func addSubscribers(){
-        dataService.$allCoins
+        // Updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else{
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { coin in
+            return coin.name.lowercased().contains(lowercasedText) ||
+                coin.symbol.lowercased().contains(lowercasedText) ||
+                coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
